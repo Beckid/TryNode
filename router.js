5928c99dc9060166253b1039a69ca953e607a94a
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-08-09 23:13:35 +0800

    add the string

--- router.js
+++ router.js
@@ -1,5 +1,5 @@
 function route(path_name) {
-	console.log('About to route for a request to' + path_name);
+	console.log('About to route for a request to ' + path_name + ' ...');
 }
 
 exports.route = route;
