7927be959b53b6067a6b0b9893e25acf47cff9eb
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-08-09 22:01:09 +0800

    Fix

--- server.js
+++ server.js
@@ -1,9 +1,12 @@
-var http = request('http');
+// Import the HTTP module.
+var http = require('http');
 
+// Start an HTTP server.
 http.createServer(function(request, response) {
 	response.writeHead(200, { "Content-Type": "text/plain" });
-	response.write("Hello, world! This is my first page using Node.js HTTP server");
+	response.write("Hello, world! This is my first page using Node.js HTTP server.");
 	response.end();
 }).listen(8888);
 
+// Print a message to the console.
 console.log("The server is running now. You can acceess it from http://localhost:8888/.");
