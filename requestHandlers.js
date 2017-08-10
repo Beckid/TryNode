25457aa4f8974ec7c873578df0dd433ade292612
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-08-10 14:33:34 +0800

    Add message to the page

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,10 +1,11 @@
 function start(response) {
 	console.log("Request handler 'start' has been enabled.");
-	response.write("");
+	response.write("Request handler 'start' has been enabled.");
 }
 
 function upload(response) {
 	console.log("Request handler 'upload' has been enabled.");
+	response.write("Request handler 'start' has been enabled.");
 }
 
 exports.start = start;
