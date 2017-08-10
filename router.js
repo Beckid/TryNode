349a788449da65479abb644a7f1ed8e89bdcf4e3
Yunpeng Niu <neilniuyunpeng@gmail.com>
2017-08-10 14:34:27 +0800

    Add line break

--- router.js
+++ router.js
@@ -3,7 +3,7 @@ function route(path_name, handle, response) {
 
 	if (typeof handle[path_name] === 'function') {
 		response.writeHead(200, { "Content-Type": "text/plain" });
-		response.write("Hello, world! This is my first page using Node.js HTTP server.");
+		response.write("Hello, world! This is my first page using Node.js HTTP server.\n");
 
 		handle[path_name](response);
 		response.end();
